43c076067f1c2f1b8eab49857945cc9e9e102298
Łukasz Antończyk <antonczyklukasz@gmail.com>
2015-10-18 09:47:08 +0000

    Add Users controller and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'pages#home'
 
   get 'help', to: 'pages#help'
-
   get 'about', to: 'pages#about'
-
   get 'contact', to: 'pages#contact'
+
+  get 'signup', to: 'users#new'
 end
